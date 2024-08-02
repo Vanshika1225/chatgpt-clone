5f0a7c8b28ae2d8719971b5145f712f16d5f56a0
vanshika1225 <vanshikakakkar1225@gmail.com>
2024-08-02 16:21:10 +0530

    side bar created

--- chatgpt-clone/src/App.js
+++ chatgpt-clone/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import gptLogo from "./assets/chatgpt.svg";
+import plusIcon from "./assets/add-30.png";
+import msgIcon from "./assets/message.svg";
+import Home from "./assets/home.svg";
+import BookmarkIcon from "./assets/bookmark.svg";
+import rocket from "./assets/rocket.svg";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar">
+        <div className="upperside">
+          <div className="uppersidetop">
+            <img src={gptLogo} alt="Chatgpt logo" className="logo" />
+            <span className="brand">ChatGpt</span>
+          </div>
+          <button className="midbtn">
+            <img src={plusIcon} alt="Plus Icon" className="addbtn" />
+            New Chat
+          </button>
+          <div className="uppersidebtn">
+            <button className="query">
+              <img src={msgIcon} alt="message icon" /> What is programming ?
+            </button>
+            <button className="query">
+              <img src={msgIcon} alt="" /> How to use an API ?
+            </button>
+          </div>
+        </div>
+        <div className="lowerside">
+          <div className="listitems">
+            {" "}
+            <img src={Home} alt="Plus Icon" className="listitemimg" />
+            Home
+          </div>
+          <div className="listitems">
+            {" "}
+            <img src={BookmarkIcon} alt="Plus Icon" className="listitemimg" />
+            Sved
+          </div>
+          <div className="listitems">
+            {" "}
+            <img src={rocket} alt="Plus Icon" className="listitemimg" />
+            Upgrade to Pro
+          </div>
+        </div>
+      </div>
+      <div className="main"></div>
     </div>
   );
 }
